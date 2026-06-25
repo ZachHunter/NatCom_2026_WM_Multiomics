ab437429e317afcb06e924e045004472a9d7f495
Zachary Hunter <ZachHunter@users.noreply.github.com>
2026-06-25 13:55:00 -0400

    Update NC_2026_Figure8.R
    code updates for figure 8 as a part of revision 2

--- NC_2026_Figure8.R
+++ NC_2026_Figure8.R
@@ -4,6 +4,9 @@ library(purrr)
 library(bvt)
 library(edgeR)
 library(maftools)
+library(emmeans)
+library(lmtest)
+library(sandwich)
 
 #============================#
 # The Evolution and Subtypes of Waldenstrom Macroglobulinemia:
@@ -11,6 +14,7 @@ library(maftools)
 #
 # This R Script includes code used to generate:
 #   Figure panel 8
+#   Supplementary table 11
 #
 # Special note - Uses r package bvt for plotting
 # Bioconductor visualization tools (bvt) is available at https://github.com/ZachHunter/bvt
@@ -76,12 +80,12 @@ names(HighP)<-MYD88$ID
 ##############################
 
 # This is combining the Early/Late EScore with Purity into a single factor for testing
-VAF_EL_Fact<-factor(paste0(pData(studyTpM)[MYD88$ID,"EScore_EL"],HighP))
+VAF_5W_Fact<-factor(paste0(pData(studyTpM)[MYD88$ID,"EScore_5W"],HighP))
 
 # Plotting CD4
 pdf(file=file.path(outputDir,"Figures/Figure8/F8A_CD4_MYD88VAF_EScore.pdf"), width = 6, height = 7)
 genePlot(studyTpM[,MYD88$ID], "CD4",
-         group="EScore_EL",
+         group="EScore_5W",
          subgroup=HighP,
          logScale=2,
          groupLabSize=1.3,
@@ -89,12 +93,11 @@ genePlot(studyTpM[,MYD88$ID], "CD4",
          ylab="Transcripts per Million +1 (Log 2 Scale)",
          main="", RSOverride=TRUE)
 dev.off()
-pairwise.wilcox.test(exprs(studyTpM)[fData(studyTpM)$GeneSymbol=="CD4",MYD88$ID],VAF_EL_Fact)
 
 # Plotting CD33
 pdf(file=file.path(outputDir,"Figures/Figure8/F8A_CD33_MYD88VAF_EScore.pdf"),width = 6, height = 7)
 genePlot(studyTpM[,MYD88$ID], "CD33",
-         group="EScore_EL",
+         group="EScore_5W",
          subgroup=HighP,
          logScale=2,
          ylab="Transcripts per Million +1 (Log 2 Scale)",
@@ -102,12 +105,11 @@ genePlot(studyTpM[,MYD88$ID], "CD33",
          axisLabelSize=1.2,
          main="", RSOverride=TRUE)
 dev.off()
-pairwise.wilcox.test(exprs(studyTpM)[fData(studyTpM)$GeneSymbol=="CD33",MYD88$ID],VAF_EL_Fact)
 
 # Plotting CD34
 pdf(file=file.path(outputDir,"Figures/Figure8/F8A_CD34_MYD88VAF_EScore.pdf"), width = 6, height = 7)
 genePlot(studyTpM[,MYD88$ID], "CD34",
-         group="EScore_EL",
+         group="EScore_5W",
          subgroup=HighP,
          logScale=2,
          ylab="Transcripts per Million +1 (Log 2 Scale)",
@@ -115,10 +117,121 @@ genePlot(studyTpM[,MYD88$ID], "CD34",
          axisLabelSize=1.2,
          main="", RSOverride=TRUE)
 dev.off()
-pairwise.wilcox.test(exprs(studyTpM)[fData(studyTpM)$GeneSymbol=="CD34",MYD88$ID],VAF_EL_Fact)
+
+##############################
+# Supplementary table 11
+# Statistical analysis of the impact of MYD88 VAF
+# on EScore Associated gene expression
+##############################
+
+# All EScore genes explicitly discussed in manuscript
+genesSets<-c("S100A9","CXCL1","CXCL8","CXCL12","CD38","SDC1","CD19","MS4A1","RAG1","MYB","IGLL1","CD3E","CD8A","CD4","CD33","FCGR3A","CD14","CD34","KIT","PROM1")
+
+# First we will contrast samples with 0.4 ≤ MYD88 VAF ≤ 0.6 vs all others at each EScore level for each gene
+geneStats<-lapply(genesSets, function(g) {
+  a<-map_dbl(paste0("ESL",1:5), function(x) {
+    IDS<-MYD88$ID[pData(studyTpM)[MYD88$ID,"EScore_5W"]==x]
+    wilcox.test(exprs(studyTpM)[fData(studyTpM)$GeneSymbol==g,IDS] ~ factor(HighP[IDS]))$p.value
+  }) %>%
+    p.adjust(method = "fdr")
+  names(a)<-paste0("ESL",1:5)
+  a
+})
+names(geneStats)<-genesSets
+
+as.data.frame(geneStats) %>%
+  t() %>%
+  kbl(caption = "FDR adjusted Wilcoxan tests of gene expression by EScore Level stratified by 0.4 ≤ MYD88 VAF ≤ 0.6") %>%
+  kable_classic(full_width=FALSE)  %>%
+  column_spec(column=1, italic = TRUE) %>%
+  as_image(file = file.path(outputDir,"Figures/STables/ST11_EScoreGenes_HighPurity_by_EScore.pdf"), width=6)
+
+# As these genes are typically log linear with EScore, we can use linear models
+# contrasting high purity status with continuous EScore or EScore levels useing
+# estimated marginal means testing.
+
+# Making continuous EScore models
+modelList<-lapply(genesSets, function(g) {
+  dat<-data.frame(
+    L2TpM=log(exprs(studyTpM)[fData(studyTpM)$GeneSymbol==g,names(HighP)] +1 ,2),
+    HighP,
+    EScore=pData(studyTpM)[names(HighP),"EScore"]
+  )
+  lm(L2TpM ~ HighP + EScore, data=dat)
+})
+
+# Making discrete EScore models with ESL1-5
+modelListLevel<-lapply(genesSets, function(g) {
+  dat<-data.frame(
+    L2TpM=log(exprs(studyTpM)[fData(studyTpM)$GeneSymbol==g,names(HighP)] +1 ,2),
+    HighP,
+    ESL=pData(studyTpM)[names(HighP),"EScore_5W"]
+  )
+  lm(L2TpM ~ HighP + ESL, data=dat)
+})
+
+#quick check to review models and make sure everything is OK
+lapply(modelList, function(x) summary(x))
+lapply(modelListLevel, function(x) summary(x))
+
+# Lets look at the estimated marginal means results per gene
+EMM_Genes<-map_dbl(modelList, function(m) {
+  if(bptest(m)$p.value<0.05) {
+    Mvcov<-vcovHC(m, type = "HC3")
+    test(emmeans(m, pairwise ~ HighP, vcov. = Mvcov)$contrasts)[[6]]
+  } else {
+    test(emmeans(m, pairwise ~ HighP)$contrasts)[[6]]
+  }
+}) %>% p.adjust("fdr")
+names(EMM_Genes)<-genesSets
+
+# Same thing but using the discrete EScore model
+EMM_Genes_ESL<-map_dbl(modelListLevel, function(m) {
+  if(bptest(m)$p.value<0.05) {
+    Mvcov<-vcovHC(m, type = "HC3")
+    test(emmeans(m, pairwise ~ HighP, vcov. = Mvcov)$contrasts)[[6]]
+  } else {
+    test(emmeans(m, pairwise ~ HighP)$contrasts)[[6]]
+  }
+}) %>% p.adjust("fdr")
+
+names(EMM_Genes_ESL)<-genesSets
+data.frame(EScore=EMM_Genes,ESLevel=EMM_Genes_ESL) %>%
+  kbl(caption = "FDR adjusted estimated marginal means testing of log 2 gene expression by 0.4 ≤ MYD88 VAF ≤ 0.6 and EScore or EScore Level (ESLevel)") %>%
+  kable_classic(full_width=FALSE)  %>%
+  column_spec(column=1, italic = TRUE) %>%
+  as_image(file = file.path(outputDir,"Figures/STables/ST11_EMM_HighPurity_by_EScore.pdf"), width=6)
+
+
+# Now we will perform the reciprocal test using continuous MYD88 VAF and Early vs. Late EScore
+modelEL_VAF<-lapply(genesSets, function(g) {
+  dat<-data.frame(
+    L2TpM=log(exprs(studyTpM)[fData(studyTpM)$GeneSymbol==g,MYD88$ID] +1 ,2),
+    VAF=MYD88$VAF,
+    ESL=pData(studyTpM)[MYD88$ID,"EScore_EL"]
+  )
+  lm(L2TpM ~ ESL + VAF, data=dat)
+})
+
+#quick check to make sure these models look OK
+lapply(modelEL_VAF, function(x) summary(x))
+
+EMM_Genes_EL_VAFL<-map_dbl(modelEL_VAF, function(m) {
+  if(bptest(m)$p.value<0.05) {
+    Mvcov<-vcovHC(m, type = "HC3")
+    test(emmeans(m, pairwise ~ ESL, vcov. = Mvcov)$contrasts)[[6]]
+  } else {
+    test(emmeans(m, pairwise ~ ESL)$contrasts)[[6]]
+  }
+}) %>% p.adjust("fdr")
+
+# The least significant gene for EScore was CD20 (MS4A1) and was still p <0.0007)
+max(EMM_Genes_EL_VAFL)
+genesSets[which(EMM_Genes_EL_VAFL==max(EMM_Genes_EL_VAFL))]
 
 
 ##############################
 # Figure 8B
 # Path Figure Panel - No R Code
 ##############################
+       
